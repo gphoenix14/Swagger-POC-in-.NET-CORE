0d8e9e573348e9b2eb62c3da8da64c3ffd4b095e
gphoenix14 <104388188+gphoenix14@users.noreply.github.com>
2024-06-08 22:34:47 +0200

    update

--- obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab8f29a0e518ec263b4888c4657b8fcba2ae8bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
